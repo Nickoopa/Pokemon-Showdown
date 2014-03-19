d96a23328cb8c331703835bb68d3ed86a9fa0f75
EnerG218 <PDManiac2212@gmail.com>
2014-03-19 10:55:37 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -747,7 +747,7 @@ var commands = exports.commands = {
 	 		if (badges.indexOf('poison') >= 0) glbadge = glbadge + '<img title="Poison" src="http://i1305.photobucket.com/albums/s542/TheBattleTowerPS/018_zps7add8bf3.png"/>';
 	 		if (badges.indexOf('kozman') >= 0) e4badge = e4badge + '<img title="Elite Four Kozman (Fighting)" src="http://i1305.photobucket.com/albums/s542/TheBattleTowerPS/145_zps5de2fc9e.png"/>';
 	 		if (badges.indexOf('solor') >= 0) e4badge = e4badge + '<img title="Elite Four Solor (Flying)" src="http://i1305.photobucket.com/albums/s542/TheBattleTowerPS/148_zpsc03fd480.png"/>';
-	 		if (badges.indexOf('zukushiku') >= 0) e4badge = e4badge + '<img title="Elite Four Zukushiku (Dark)" src="http://i1305.photobucket.com/albums/s542/TheBattleTowerPS/078_zpsda18a62b.png"/>';
+	 		if (badges.indexOf('zukushiku') >= 0) e4badge = e4badge + '<img title="Elite Four Zukushiku (Steel)" src="http://i1305.photobucket.com/albums/s542/TheBattleTowerPS/134_zpsf585594f.png"/>';
 	 		if (badges.indexOf('marlon') >= 0) e4badge = e4badge + '<img title="Elite Four Marlon (Water)" src="http://i.imgur.com/FYHMALw.png"/>';
 	 		if (glbadge == '' && e4badge == '') return self.sendReplyBox(username+' has no badges.');
 	 		self.sendReplyBox('<center>'+username+'\'s badges <br /><br />Gym Leader Badges:<br />'+glbadge+'<br />Elite Four Badges: <br />'+e4badge+'</center>');
